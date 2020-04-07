6ed2f63011ca7eb0409cf16e3b940b0a1d836005
Tarik <tariksnow37@gmail.com>
2020-04-06 17:29:52 -0700

    adding baking

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 // Intro to PSO
+
+//Testing
+let baking;
+
+//Day 1 --
+// let hiking;
+
+//Day 2 --
+// let beej;
+
+//Day 3 --
+// let coding;
+
+//Day 4 -- 
+// let thanks;
+
+// Day 5 --
+// let happy
